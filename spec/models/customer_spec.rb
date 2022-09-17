66abb6f124625ddf60ebd5fc3c79eeae3e354e9a
Wesley <matthew282@msn.com>
2022-09-17 14:28:01 -0700

    add formatting and basic tests

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+
+  subject {Customer.new(first_name: "Jack", last_name: "Smith", phone: "8889995678", email: "jsmith@sample.com")}
+
+    it "is valid with valid attributes" do 
+      expect(subject).to be_valid
+    end
+
+    it "is not valid without a first_name" do
+      subject.first_name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a last_name" do
+      subject.last_name = nil
+      expect(subject).to_not be_valid
+    end
 end
