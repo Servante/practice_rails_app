90a3ab7be179808f503ec6f8d4406ad51958c8b5
Wesley <matthew282@msn.com>
2022-10-01 23:02:03 -0700

    add customer model and tests

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Customer, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
 
-  subject {Customer.new(first_name: "Jack", last_name: "Smith", phone: "8889995678", email: "jsmith@sample.com")}
+  subject {Customer.new(first_name: "Jack", last_name: "Smith", phone: "8889995678")}
 
     it "is valid with valid attributes" do 
       expect(subject).to be_valid
@@ -17,5 +17,18 @@ RSpec.describe Customer, type: :model do
     it "is not valid without a last_name" do
       subject.last_name = nil
       expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a phone number" do
+      subject.phone = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid if the phone number is not 10 chars" do
+      expect(subject.phone.length).to eq(10)
+    end
+
+    it "is not valid if the phone number is not all digits" do
+      expect((subject.phone).match?(/\A-?\d+\z/)).to eq(true)
     end
 end
