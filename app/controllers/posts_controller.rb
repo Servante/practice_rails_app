27d46af459ac2d46295ee6bbff03a41ce698ea77
Wesley <matthew282@msn.com>
2022-10-14 10:43:56 -0700

    update crud logic

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,18 +19,29 @@ class PostsController < ApplicationController
     end
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
   def edit
     @post = Post.find(params[:id])
   end
 
   def update
-  end
-
-  def show
     @post = Post.find(params[:id])
-  end
 
-  def destroy    
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit, status: unprocessable_entity
+    end
+  end 
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to root_path, status: :see_other
   end
 
   private
